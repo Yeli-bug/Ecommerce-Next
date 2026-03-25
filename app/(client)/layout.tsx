f4c3936304d8ebc9893773310234509846d4c04a
Yeli-bug <76750335+Yeli-bug@users.noreply.github.com>
2026-03-24 18:12:38 -0600

    What’s Covered in Part 3:* How to add Clerk in nextjs Implementing Sign in and Sign out method Setup sanity in nextjs project Creating sanity schemas Importing seed data in sanity. Refactor code structure for improved readability and maintainability

---
+++ app/(client)/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import "../globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { ClerkProvider } from '@clerk/nextjs'
+
+
+export const metadata: Metadata = {
+  title: "Diseños Emperador",
+  description: "Uniformes escolares y deportivos.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+          <div className="flex flex-col min-h-screen">
+            <Header/>
+            <main className="flex-1">
+              {children}
+            </main>
+            <Footer/>
+          </div>
+    </ClerkProvider>
+  );
+}
+
+//explicame que es y para que sirve:
+//RootLayout: es el layout principal de toda tu aplicación.
+//children: Es todo lo que se renderiza dentro del layout.
+//React.ReactNode: Es un tipo de dato en React. 
+
+//explicame los estilos: flex flex-col min-h-screen
+//display: flex; Activa Flexbox. Permite controlar layout (alineación, distribución, etc.)
+//flex-direction: column. Los elementos se acomodan verticalmente.
+//min-height: 100vh. Altura mínima = toda la pantalla
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,10 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import { ClerkProvider } from '@clerk/nextjs'
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+    return ( 
+    <html lang="en">
+        <body className="antialiased">{children}</body>
+        </html>
+    );
+};  
+export default RootLayout;
 
-
-export const metadata: Metadata = {
-  title: "Diseños Emperador",
-  description: "Uniformes escolares y deportivos.",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <ClerkProvider>
-      <html lang="en">
-        <body>
-          <div className="flex flex-col min-h-screen">
-            <Header/>
-            <main className="flex-1">
-              {children}
-            </main>
-            <Footer/>
-          </div>
-        </body>
-      </html>
-    </ClerkProvider>
-  );
-}
-
-//explicame que es y para que sirve:
-//RootLayout: es el layout principal de toda tu aplicación.
-//children: Es todo lo que se renderiza dentro del layout.
-//React.ReactNode: Es un tipo de dato en React. 
-
-//explicame los estilos: flex flex-col min-h-screen
-//display: flex; Activa Flexbox. Permite controlar layout (alineación, distribución, etc.)
-//flex-direction: column. Los elementos se acomodan verticalmente.
-//min-height: 100vh. Altura mínima = toda la pantalla
+//Explicame toda este codigo: 
